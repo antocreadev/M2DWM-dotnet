befb4556a34ddf1d7d86545e9d6835b4cc2f6175
Frédéric COLIN <frederic.colin@equadex.net>
2025-09-10 16:51:05 +0200

    Xml2

---
+++ M2Corte2025.FormationFred.Services/Xml2RecipesServices.cs
@@ -0,0 +1,21 @@
+﻿using M2Corte2025.FormationFred.DataContracts;
+using M2Corte2025.FormationFred.ServicesContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace M2Corte2025.FormationFred.Services
+{
+    public class Xml2RecipesServices : AbstractRecipesServices
+    {
+        public override List<Recipe> GetAll()
+        {
+            XDocument xdoc = XDocument.Load("recipes.xml");
+
+            return xdoc.Descendants("recipe").Select(@recipe => new Recipe() { Id = Guid.Parse(@recipe.Attribute("id").Value), Title = recipe.Attribute("title").Value }).ToList();
+        }
+    }
+}
