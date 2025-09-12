650dc0d5e75ceb093530bb173accd86ca8a58a7b
Frédéric COLIN <frederic.colin@equadex.net>
2025-09-12 10:39:51 +0200

    Facto DB01

--- M2Corte2025.FormationFred.Services/Db01RecipesServices.cs
+++ M2Corte2025.FormationFred.Services/Db01RecipesServices.cs
@@ -15,61 +15,15 @@ namespace M2Corte2025.FormationFred.Services
         public override List<Recipe> GetAll()
         {
             var cs = "RecipesConnectionString".GetConfigValueFor("ConnectionStrings");
-            var recipes = new List<Recipe>();
 
-            using (var cn = new SqlConnection(cs))
-            {
-                cn.Open();
-
-                var cmd = cn.CreateCommand();
-
-                cmd.CommandText = "SELECT * FROM tRecipes";
-                cmd.CommandType = System.Data.CommandType.Text;
-
-                var reader = cmd.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    var id = Guid.Parse(reader["Id"].ToString());
-                    var title = reader["Title"].ToString();
-
-                    recipes.Add(
-                        new Recipe() { Id = id, Title=title}
-                        );
-                }
-
-                return recipes;
-            }
+            return getInternalRecipes(cs, "SELECT * FROM tRecipes", System.Data.CommandType.Text);
         }
 
         public override List<Recipe> GetByTitle(string ptitle)
         {
             var cs = "RecipesConnectionString".GetConfigValueFor("ConnectionStrings");
-            var recipes = new List<Recipe>();
 
-            using (var cn = new SqlConnection(cs))
-            {
-                cn.Open();
-
-                var cmd = cn.CreateCommand();
-
-                cmd.CommandText = $"SELECT * FROM tRecipes WHERE Title LIKE '%' + '{ptitle}' + '%'";
-                cmd.CommandType = System.Data.CommandType.Text;
-
-                var reader = cmd.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    var id = Guid.Parse(reader["Id"].ToString());
-                    var title = reader["Title"].ToString();
-
-                    recipes.Add(
-                        new Recipe() { Id = id, Title = title }
-                        );
-                }
-
-                return recipes;
-            }
+            return getInternalRecipes(cs, $"SELECT * FROM tRecipes WHERE Title LIKE '%' + '{ptitle}' + '%'", System.Data.CommandType.Text);
         }
     }
 }
--- M2Corte2025.FormationFred.ServicesContracts/AbstractRecipesServices.cs
+++ M2Corte2025.FormationFred.ServicesContracts/AbstractRecipesServices.cs
@@ -1,6 +1,8 @@
 ﻿using M2Corte2025.FormationFred.DataContracts;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,5 +15,33 @@ namespace M2Corte2025.FormationFred.ServicesContracts
 
         public abstract List<Recipe> GetByTitle(String title);
 
+        protected List<Recipe> getInternalRecipes(String recipesConnectionString, String commandText, CommandType commandType)
+        {
+            var recipes = new List<Recipe>();
+
+            using (var cn = new SqlConnection(recipesConnectionString))
+            {
+                cn.Open();
+
+                var cmd = cn.CreateCommand();
+
+                cmd.CommandText = commandText;
+                cmd.CommandType = commandType;
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var id = Guid.Parse(reader["Id"].ToString());
+                    var title = reader["Title"].ToString();
+
+                    recipes.Add(
+                        new Recipe() { Id = id, Title = title }
+                        );
+                }
+
+                return recipes;
+            }
+        }
     }
 }
