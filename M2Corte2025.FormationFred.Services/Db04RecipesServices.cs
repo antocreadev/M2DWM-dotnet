c5e9126b8c1a041c5c2924b413eb612255f725e3
Frédéric COLIN <frederic.colin@equadex.net>
2025-09-12 16:20:16 +0200

    Db04

---
+++ M2Corte2025.FormationFred.Services/Db04RecipesServices.cs
@@ -0,0 +1,26 @@
+﻿using M2Corte2025.FormationFred.DataContracts;
+using M2Corte2025.FormationFred.Services.Core;
+using M2Corte2025.FormationFred.ServicesContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2Corte2025.FormationFred.Services
+{
+    public class Db04RecipesServices : AbstractRecipesServices
+    {
+        public override List<Recipe> GetAll()
+        {
+            var cs = "AzureRecipesConnectionString".GetConfigValueFor("ConnectionStrings");
+            return getInternalRecipes(cs, "sSelectRecipes", System.Data.CommandType.StoredProcedure);
+        }
+
+        public override List<Recipe> GetByTitle(string title)
+        {
+            var cs = "AzureRecipesConnectionString".GetConfigValueFor("ConnectionStrings");
+            return getInternalRecipes(cs, "sSelectRecipes", System.Data.CommandType.StoredProcedure, title);
+        }
+    }
+}
